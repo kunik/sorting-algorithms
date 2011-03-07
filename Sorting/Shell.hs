5400a767ece24e8841979a0305b741e52d24dc46
Taras Kunch <taras.kunch@gmail.com>
2011-03-07 13:13:32 +0200

    Performed small refactoring in Shell algorithm implementation

--- Sorting/Shell.hs
+++ Sorting/Shell.hs
@@ -9,14 +9,15 @@ import Data.List (transpose, unfoldr)
 -- Each phase of the Shell sort breaks the list into k columns, sorts each with an
 -- insertion sort, then merges those columns back into a list.
  
-columnize :: Int -> [a] -> [[a]]
-columnize k = transpose . takeWhile (not . null) . unfoldr (Just . splitAt k)
- 
-decolumnize :: [[a]] -> [a]
-decolumnize = concat . transpose
- 
 shellSortPhase :: (Ord a) => Int -> [a] -> [a]
 shellSortPhase k = decolumnize . map (I.sort) . columnize k 
+    where 
+        columnize :: Int -> [a] -> [[a]]
+        columnize k = transpose . takeWhile (not . null) . unfoldr (Just . splitAt k)
+         
+        decolumnize :: [[a]] -> [a]
+        decolumnize = concat . transpose
+
  
 sort :: (Ord a) => [a] -> [a]
 sort xs = foldr shellSortPhase xs gaps
